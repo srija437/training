6c5d8633c4ff7403c358d7fbfc61e724cf97e3e0
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2025-12-24 10:14:36 +0530

    Initial commit

---
+++ Matrix.java
@@ -0,0 +1,17 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+        };
+        System.out.println("Matrix elements:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ StringMethods.java
@@ -0,0 +1,12 @@
+public class StringMethods {
+    public static void main(String[] args){
+        String s = "Srija";
+        s=s.concat("Reddy");
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(7));
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(3, 7));
+
+    }
+}
