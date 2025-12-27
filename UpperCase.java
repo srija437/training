4a332e40d1950ef3e1b75c6e5ca106a1c5ed1cf1
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2025-12-27 11:01:50 +0530

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,8 @@
+public class UpperCase {
+    public static void main(String[] args){
+        String s1 = "java";
+        String s2 = "JAVA";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s2.toLowerCase());
+    }
+}
---
+++ trim.java
@@ -0,0 +1,7 @@
+public class trim {
+    public static void main(String[] args) {
+        String s1 = " Srija Reddy ";
+        System.out.println(s1.trim());
+
+    }
+}
