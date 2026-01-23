3022f289f925ae25ed576838c59e788f9d4ca8ea
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2026-01-23 14:10:08 +0530

    Add files via upload

---
+++ BankAccount2.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class BankAccount2 {
+    private double balance = 10000;
+    public static void main(String[] args) {
+        BankAccount2 obj = new BankAccount2();
+        System.out.println(obj.balance);
+    }
+}
