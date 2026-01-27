57edf97ace11c1d04c4c55397864115db4accda5
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2026-01-27 14:08:10 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+package BankA;
+
+public class BankAccount {
+    private double balance=10000;
+
+    public static void main(String[] args){
+        BankAccount obj=new BankAccount();
+        System.out.println(obj.balance);
+
+    }
+}
--- BankAccount2.java
+++ BankAccount2.java
@@ -1,9 +1,8 @@
-import java.sql.SQLOutput;
+package bankA;
 
 public class BankAccount2 {
-    private double balance = 10000;
-    public static void main(String[] args) {
-        BankAccount2 obj = new BankAccount2();
-        System.out.println(obj.balance);
+    public static void main(String[] args){
+        BankAccount2 obj=new BankAccount2();
+        //obj.balance;
     }
 }
